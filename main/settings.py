16a8848df2f0a8168d6c2155f2a252ea2c8eb1e6
azizullah2017 <p146111@nu.edu.pk>
2024-04-23 16:08:24 +0500

    added cors

--- main/settings.py
+++ main/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = "django-insecure-iw2=%ovuflz7&_9e#263$j9c3_!$0r=g=_fw@@xm#8drmhg56f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
+
+ALLOWED_HOSTS=['*']
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 
 
 # Application definition
