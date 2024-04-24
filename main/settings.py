f6ff7b09b77d5951942b81ccaae2fa7017190ccc
azizullah2017 <p146111@nu.edu.pk>
2024-04-24 17:26:12 +0500

    fixed cors

--- main/settings.py
+++ main/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "apis",
+    "corsheaders",
     "rest_framework",
     'rest_framework.authtoken',
 ]
@@ -63,6 +64,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = "main.urls"
